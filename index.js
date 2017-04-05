57c706b0168b2fe4f4185bdfadc2444a961f0959
Guillermo Orellana <guillermo.orellana@corp.badoo.com>
2017-04-05 18:34:00 +0100

    flowtype

--- index.js
+++ index.js
@@ -1,50 +1,42 @@
-import { NativeModules } from 'react-native';
+/* @flow */
+import {NativeModules} from 'react-native';
 
-const { RNSpotifyAuth } = NativeModules;
+const {RNSpotifyAuth} = NativeModules;
 
 export default class ReactNativeSpotifyAuth {
-    /**
-     * @param {String} clientId
-     * @param {String} redirectUri
-     */
-    constructor(clientId, redirectUri) {
-        this.clientId = clientId;
-        this.redirectUri = redirectUri;
-    }
-    /**
-     * @returns {Promise}
-     */
-    startLogin() : Promise {
-        return RNSpotifyAuth.launchAuth(this.clientId, this.redirectUri)
-            .then(
-                function (data) {
-                    return new SpotifyAuthData(data.token, data.code, data.error, data.state, data.expiresIn);
-                },
-                function (reason){
-                    return Promise.reject(reason);
-                }
-            );
-    }
+  clientId: string;
+  redirectUri: string;
+  /**
+   * @param {String} clientId
+   * @param {String} redirectUri
+   */
+  constructor(clientId: string, redirectUri: string) {
+    this.clientId = clientId;
+    this.redirectUri = redirectUri;
+  }
+  /**
+   * @returns {Promise<SpotifyAuthData>}
+   */
+  startLogin(): Promise<SpotifyAuthData> {
+    return RNSpotifyAuth.launchAuth(this.clientId, this.redirectUri).then(
+      function(data) {
+        return {
+          token: data.token,
+          code: data.code,
+          error: data.error,
+          state: data.state,
+          expiresIn: data.expiresIn,
+        };
+      },
+      reason => reason,
+    );
+  }
 }
 
-class SpotifyAuthData {
-    token: string;
-    code: string;
-    error: string;
-    state: string;
-    expiresIn: number;
-   /**
-     * @param {string} token
-     * @param {string} code
-     * @param {string} error
-     * @param {string} state
-     * @param {number}    expiresIn
-     */
-    constructor(token: string, code: string, error: string, state: string, expiresIn: number) {
-        this.token = token;
-        this.code = code;
-        this.error = error;
-        this.state = state;
-        this.expiresIn = expiresIn;
-    }
-}
+type SpotifyAuthData = {
+  token: string,
+  code: string,
+  error: string,
+  state: string,
+  expiresIn: number,
+};
