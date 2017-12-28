8f01410039e3ac461814e29c7cc74c3e42a99c54
Guillermo <wiyarmir@congresos-145-13.wifi.uma.es>
2017-12-28 11:19:22 +0100

    bump react and fix deprecations

--- android/src/main/java/es/wiyarmir/rnspotifyauth/SpotifyAuthPackage.java
+++ android/src/main/java/es/wiyarmir/rnspotifyauth/SpotifyAuthPackage.java
@@ -18,11 +18,6 @@ public class SpotifyAuthPackage implements ReactPackage {
     }
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
